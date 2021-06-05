b013fb7c5cab01d34d7801b216ee27d4ea845e9c
Romeo Ben Manangu <romeobenmanangu@gmail.com>
2021-06-05 13:57:43 +0800

    init

---
+++ main.py
@@ -0,0 +1,171 @@
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances, cosine_similarity
+
+from PIL import Image
+
+st.set_page_config(page_title='P-Pop to the Top!', layout="wide")
+warnings.filterwarnings('ignore')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+#prov = pd.read_csv("schools_prov.csv")
+#region = pd.read_csv("schools_region.csv", index_col="school.region")
+#merged_data = gpd.read_file('./merged_data/merged_data.shp')
+#corr = prov[['Schools_Income', 'Schools_Teachers', 'Schools_Rooms', 'Student_Teacher_Ratio', 'Schools_Enrollment']].corr()
+
+
+def project():
+    st.title('Catapulting Pinoy Pop to the Top')
+    st.subheader('by Data Science Fellowship Cohort 7 - Group 1')
+    st.write('Bym, Ben, Matt, Miggy, Nilly, Robby (mentored by Danilo)')
+
+    teacher_image = Image.open('img/teacher.jpg')
+
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        st.image(
+            teacher_image,
+        )
+    with col2:
+        st.markdown(
+            "We harnessed the power of data to decode the **TikTok** sound and inspire **SB19’s** new hit track designed to *top the charts*."
+        )
+
+     
+def sb19():
+    st.title('Introducing SB19')
+    st.write("")
+    st.subheader(
+            "Meet Josh, Pablo, Stell, Ken, and Justin"
+        )
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        st.subheader(
+            "PICTURE HERE"
+        )
+        #sb19_pic = Image.open('what_is_mooe.png')
+        #st.image(sb19_pic)
+    with col2:
+        st.markdown(
+            " - Trained for 3 years by ShowBT Philippines",
+            " - Active since 2018",
+            " - The first Filipino and Southeast Asian act to be nominated in **Billboard Music Awards** for the *Top Social Artist* category",
+        )
+    
+    st.subheader(
+            "On Social Media:"
+        )
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        st.markdown('**Facebook**')
+        st.write("**1.4M Followers**")
+
+    with col2:
+        st.subheader('**Twitter**')
+        st.write("**422k Followers**")
+
+    with col3:
+        st.subheader('**Instagram**')
+        st.write("**454K Followers**")
+
+
+def data_method():
+    st.title('Data Sources and Methodology')
+    st.write("")
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        data_sources = Image.open("data_sources.png")
+        st.image(data_sources)
+    with col2:
+        methodology = Image.open("methodology.png")
+        st.image(methodology)
+
+def methodology():
+    st.title('Methodology')
+    methodology = Image.open("methodology.png")
+    st.image(methodology)
+
+def chart_perf():
+    st.title('Chart Performance')
+    st.subheader("What are these TikTok artists doing right?")
+    st.write("")
+    st.write("Correlation coefficient: 0.42")
+
+def pitch():
+    st.title('How can we use data science to help SB19?')
+    
+
+def recommender():
+    st.title('The Recommender Engine')
+    
+
+def conclusion():
+    st.title('How to we take P-Pop to the top?')
+
+def references():
+    st.title('References')
+    
+    st.subheader('[1] Building Better Learning Environments in the Philippines')
+    st.write("World Bank Group. (2016). Building Better Learning Environments in the Philippines. Philippines education note,no. 4;. World Bank, Washington, DC. © World Bank. https://openknowledge.worldbank.org/handle/10986/24744 License: CC BY 3.0 IGO.")
+    
+    st.subheader("[2] House Bill No. 473: An Act Regulating Class Size in All Public Schools and Appointing Funds Therefor")
+    st.write("Tinio, A. L., & Castro, F. L. (2016, June 30). House Bill No. 473: An Act Regulating Class Size in All Public Schools and Appointing Funds Therefor. House Bill No. 473. https://www.congress.gov.ph/legisdocs/basic_17/HB00473.pdf.")
+    
+    st.subheader("[3] Class-size affects students' learning : DepEd. Philippine News Agency RSS")
+    st.write("Montemayor, M. T. (2018, March 19). Class-size affects students' learning : DepEd. Philippine News Agency RSS. https://www.pna.gov.ph/articles/1029281. ")
+
+    st.subheader('[4] Enhanced Basic Education Information System (EBEIS) (2015)')
+    st.write("")    
+
+    st.subheader('[5] Comparing the DISADVANTAGE INDEX (DI) with GEOGRAPHICALLY ISOLATED AND DISADVANTAGED AREAS (GIDA)')
+    st.write("Comparing the DISADVANTAGE INDEX (DI) with GEOGRAPHICALLY ISOLATED AND DISADVANTAGED AREAS (GIDA). DepEd, 2015.")      
+    
+    st.subheader('[6] Computation of Public Schools MOOE')
+    st.write("Llego, M. A. (2015). Computation of Public Schools MOOE. https://www.teacherph.com/computation-public-schools-mooe/")  
+    
+    
+    
+list_of_pages = [
+    "The Project",
+    "Who is SB19?",
+    "Data Sources and Methodology",
+    "SB19's Chart Performance",
+    "The Pitch",
+    "Genre Classifier and Recommender Engine",
+    #"The Playlists",
+    "Conclusion",
+    "References"
+]
+
+st.sidebar.title('Table of Contents')
+selection = st.sidebar.radio("Go to", list_of_pages)
+
+if selection == "The Project":
+    project()
+
+elif selection == "Who is SB19?":
+    sb19()
+
+elif selection == "Data Sources and Methodology":
+    data_method()
+
+elif selection == "SB19's Chart Performance":
+    chart_perf()
+
+elif selection == "The Pitch":
+    pitch()
+
+elif selection == "Genre Classifier and Recommender Engine":
+    recommender()
+
+elif selection == "Conclusion and Recommendations":
+    conclusion()
+    
+elif selection == "References":
+    references()
